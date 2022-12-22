e72898669a452dbc69f9e07976b2bc694fdb5bed
pe3769 <121032462+pe3769@users.noreply.github.com>
2022-12-22 07:19:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -44,22 +44,21 @@ server.on('listening', () => {
   console.log('Listening on ' + bind);
 });
 
-// Add Express
+
 const express = require("express");
 
-// Initialize Express
+
 const app = express();
 
-// Create GET request
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-// Initialize server
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-// Export the Express API
 module.exports = app;
 
 server.listen(port);
